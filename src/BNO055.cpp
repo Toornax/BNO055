b98a1ed71e403addddbf7cc77258ee534391bcd4
Toornax <nathan.leloup.t@gmail.com>
2019-02-03 18:13:00 +0100

    Base files

---
+++ src/BNO055.cpp
@@ -0,0 +1,6 @@
+#include "BNO055.hpp"
+#include <Arduino.h>
+
+void test() {    
+  digitalWrite(13, HIGH);
+}
---
+++ src/BNO055.hpp
@@ -0,0 +1,6 @@
+#ifndef BNO055_HPP
+#define BNO055_HPP
+
+void test();
+
+#endif // BNO055_HPP
